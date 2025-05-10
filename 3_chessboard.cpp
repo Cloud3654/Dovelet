1cf95f0e9194a0fe3f21dd42ec42894c1d1f5624
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-05-11 08:08:25 +0900

    Update 3_chessboard.cpp

--- 3_chessboard.cpp
+++ 3_chessboard.cpp
@@ -5,7 +5,7 @@ int main() {
     int num; cin >> num;
     int sp = sqrt(num - 1);
     int middle = ((sp * sp + 1) + ((sp + 1) * (sp + 1))) / 2;
-    int x = sp + 1, y = sp + 1 - abs(p - middle);
+    int x = sp + 1, y = sp + 1 - abs(num - middle);
     if ((sp + 1) % 2) swap(x, y);
     if (num < middle) swap(x, y);
     cout << x << " " << y;
